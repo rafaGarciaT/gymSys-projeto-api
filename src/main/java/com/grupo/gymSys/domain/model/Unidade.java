2941ffc8062ba9a9a1b0f89b59a81f224ab06eac
dias_guilherme01 <Sou.guilhermedias@gmail.com>
2025-05-22 20:46:25 -0300

    Fazendo a parte da Unidade.java get e set adicionados

--- src/main/java/com/grupo/gymSys/domain/model/Unidade.java
+++ src/main/java/com/grupo/gymSys/domain/model/Unidade.java
@@ -5,4 +5,62 @@ import jakarta.persistence.*;
 @Entity
 @Table(name= "tb_unidades")
 public class Unidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String endereco;
+
+    private String cidade;
+    private String estado;
+
+    private String numero;
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
 }
