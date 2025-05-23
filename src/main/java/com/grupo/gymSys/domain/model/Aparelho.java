3e0f4d2e12b6ad83e7886bee44d6ea7c4b8c6d6c
andre_nicolas <billymarrygina@gmail.com.br>
2025-05-22 21:05:11 -0300

    adicionei a parte do Aparelho.java

--- src/main/java/com/grupo/gymSys/domain/model/Aparelho.java
+++ src/main/java/com/grupo/gymSys/domain/model/Aparelho.java
@@ -5,4 +5,45 @@ import jakarta.persistence.*;
 @Entity
 @Table(name= "tb_aparelhos")
 public class Aparelho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String tipo;
+    private int quantidade;
+    private String ultimaMenutencao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUltimaMenutencao() {
+        return ultimaMenutencao;
+    }
+
+    public void setUltimaMenutencao(String ultimaMenutencao) {
+        this.ultimaMenutencao = ultimaMenutencao;
+    }
+
+
 }
