038e3ebbf3d69e575901f0dd49874b2b2d5ff549
dias_guilherme01 <Sou.guilhermedias@gmail.com>
2025-05-29 21:14:35 -0300

    fazendo UnidadeServiceImpl.java faltando detalhes

--- src/main/java/com/grupo/gymSys/service/UnidadeService.java
+++ src/main/java/com/grupo/gymSys/service/UnidadeService.java
@@ -1,6 +1,12 @@
 package com.grupo.gymSys.service;
 
 import com.grupo.gymSys.domain.model.Unidade;
+import com.grupo.gymSys.domain.model.Usuario;
 
 public interface UnidadeService {
+    Unidade findById(Long id);
+
+    Unidade create(Usuario userToCreate);
+
+    void deleteById(Long id);
 }
--- src/main/java/com/grupo/gymSys/service/impl/UnidadeServiceImpl.java
+++ src/main/java/com/grupo/gymSys/service/impl/UnidadeServiceImpl.java
@@ -1,12 +1,38 @@
 package com.grupo.gymSys.service.impl;
 
 import com.grupo.gymSys.domain.model.Unidade;
+import com.grupo.gymSys.domain.model.Usuario;
 import com.grupo.gymSys.domain.repository.UnidadeRepository;
+import com.grupo.gymSys.domain.repository.UsuarioRepository;
 import com.grupo.gymSys.service.UnidadeService;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
-public class UnidadeServiceImpl {
+public class UnidadeServiceImpl implements UnidadeService {
+
+    private final UnidadeRepository unidadeRepository;
+
+    public UnidadeServiceImpl(UnidadeRepository unidadeRepository) {
+        this.unidadeRepository = unidadeRepository;
+    }
+    @Override
+    public Unidade findById(Long id) {
+        return unidadeRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Unidade create(Usuario userToCreate) {
+        if(unidadeRepository.existsById(Long id)unidadeTocreate.getid){}
+        return unidadeRepository.save(unidadeToCreate);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (!unidadeRepository.existsById(id)) {
+            throw new NoSuchElementException("Nenhuma unidade encontrado com esse id.");
+        }
+        unidadeRepository.deleteById(id);
+    }
 }
