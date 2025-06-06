49985890e2dad4e411fd62974423bc48b2f74d20
andre_nicolas <billymarrygina@gmail.com>
2025-06-05 21:12:53 -0300

    atualizei aparelho

--- src/main/java/com/grupo/gymSys/domain/model/Aparelho.java
+++ src/main/java/com/grupo/gymSys/domain/model/Aparelho.java
@@ -1,6 +1,9 @@
 package com.grupo.gymSys.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.List;
 
 @Entity
 @Table(name= "tb_aparelhos")
@@ -8,10 +11,23 @@ public class Aparelho {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    @PositiveOrZero(message = "Campo Id precisa ser 0 ou positivo")
+
     private long id;
+
+    @NotBlank(message = "Campo X obrigatório.")
+    @Size(max = 100, message = "X não pode passar de 100 caracteres.")
+
     private String tipo;
+
+    @NotNull(message = "Campo X obrigatório")
+    @Positive(message = "X precisa ser positivo")
+
     private int quantidade;
     private String UltimaMenutencao;
+
+    @OneToMany(mappedBy = "aparelho")
+    private List<UnidadeAparelho> unidadeAparelhos;
 
     public long getId() {
         return id;
